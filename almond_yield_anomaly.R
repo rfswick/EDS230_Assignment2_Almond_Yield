8c738e794c95e903d4dd9c30fa5ecd0d5d4a1f76
rfswick <rachelfswick@gmail.com>
2025-04-13 23:29:16 -0700

    Update function and documentation

--- almond_yield_anomaly.R
+++ almond_yield_anomaly.R
@@ -1,7 +1,14 @@
-#' A function to report on my dog's weight
+#' A function to return the minimum, maximum, and average almond yield anomaly over a given time series.
+#' This function was created for EDS 230, Environmental Modeling on April 13, 2025.
+#' @author Rachel Swick and Ryan Green
 #'
-#' @param filepath The file path to a `txt` file with climate data for a time series. 
-#' 
+#' @param filepath The file path to a `txt` file with climate data for a time series.
+#' The climate data file must include the following columns:
+#' @field day day of the month the climate data was recorded on, numeric 
+#' @field month number of the month the climate data was recorded in, numeric
+#' @field year year the climate data was recorded in, numeric
+#' @field tmin_c daily minimum temperature in degrees Celsius, numeric
+#' @field precip amount of precipitation per day in millimeters, numeric
 #'
 #' @return
 #' @export summary_yield_anomaly A table with the minimum, maximum, and average almond yield anomaly for a given time series
