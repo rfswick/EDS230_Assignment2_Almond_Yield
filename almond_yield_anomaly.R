c25251acdb5d1287304699b188c082cd109620c8
rfswick <rachelfswick@gmail.com>
2025-04-13 22:28:45 -0700

    Function documentation

--- almond_yield_anomaly.R
+++ almond_yield_anomaly.R
@@ -1,14 +1,22 @@
-almond_yield <- function(filename = "data/clim.txt") {
+#' A function to report on my dog's weight
+#'
+#' @param filepath The file path to a `txt` file with climate data for a time series. 
+#' 
+#'
+#' @return
+#' @export summary_yield_anomaly A table with the minimum, maximum, and average almond yield anomaly for a given time series
+#'
+#' @examples
+#' almond_yield_anomaly("data/clim.txt")
+
+almond_yield_anomaly <- function(filepath) {
   
   # This function created by Rachel Swick and Ryan Green
   # For EDS 230
   # 4/13/25
   
   # Read in the data
-  data <- read.table(filename, header = TRUE, sep = " ")
-  
-  # Load necessary package
-  library(dplyr)
+  data <- read.table(filepath, header = TRUE, sep = " ")
   
   # Aggregate February data: get the minimum temp (tmin_c) for February per year
   data_february <- data %>%
@@ -32,14 +40,14 @@ almond_yield <- function(filename = "data/clim.txt") {
     mutate(yield_anomaly = -0.015 * mintemp - 0.0046 * (mintemp^2) - 0.07 * total_precip + 0.0043 * (total_precip^2) + 0.28)
   
   # Summarize the yield anomaly statistics
-  summary_yield <- data_clim %>%
+  summary_yield_anomaly <- data_clim %>%
     summarise(
       min_yield = min(yield_anomaly, na.rm = TRUE),
-      max_yield = max(yield_anomaly, na.rm = TRUE),
-      mean_yield = mean(yield_anomaly, na.rm = TRUE))
+      mean_yield = mean(yield_anomaly, na.rm = TRUE),
+      max_yield = max(yield_anomaly, na.rm = TRUE))
   
   # Print the summary statistics
-  print(summary_yield)
+  return(summary_yield_anomaly)
 }
 
-almond_yield("data/clim.txt")
+
